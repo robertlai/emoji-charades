061b3c4beb4b7c2a11140ea0334e8f6b8a39d21c
Robert Lai <rcylai@hotmail.com>
2024-05-07 01:53:27 -0400

    Add start button to lobby

--- client/components/Lobby/Lobby.js
+++ client/components/Lobby/Lobby.js
@@ -6,12 +6,16 @@ import styles from "./Lobby.module.scss";
 
 export default function Lobby() {
   const { gameState } = useContext(SocketContext);
+  const canStart = gameState.players.length > 1;
 
   return (
     <main className={styles.main}>
       <div className={styles.hero}>
         <HeroGraphic />
         <h1>Emoji Charades</h1>
+        <button className={!canStart && styles.disabled} disabled={!canStart}>
+          Start
+        </button>
       </div>
       <div className={styles.players}>
         {gameState.players.map(({ id, name, avatarUri }) => (
